c9b8ca122e3b99c57afe1e7904654ba04b28f549
sneha-thammineni <36546445+sneha-thammineni@users.noreply.github.com>
2018-04-03 18:15:46 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+function primesTimesTable(num){
+    
+       
+    for(var i=1;i<=10;i++){
+        
+       var result = num*i;   
+        console.log(num + " * " + i + " = " + result);
+        
+    } 
+
+}
+primesTimesTable(5);
